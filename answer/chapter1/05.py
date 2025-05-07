ba8d236eff617ec5442e09f29dd5f7cefa96ace0
odoriko-yoru <im.shu.hawking314@gmail.com>
2025-05-08 01:16:21 +0900

    chapter1 #3

---
+++ answer/chapter1/05.py
@@ -0,0 +1,29 @@
+from typing import Union
+
+
+def create_n_gram(n: int, sequence: Union[str, list]) -> list[str]:
+    """Separate inputed sentence and output a char/word n-gram.
+
+    Parameters
+    ----------
+    n : int
+        Number of gram.
+
+    sequence : str or list
+        Inputed sentence
+
+    Returns
+    -------
+    tuple[str]
+    """
+    return [sequence[i : i + n] for i in range(len(sequence) - n + 1)]
+
+
+sentence = "I am an NLPer"
+
+for i in range(1, 4):
+    # n-gram of the character
+    print(create_n_gram(i, sentence))
+
+    # n-gram of words
+    print(create_n_gram(i, sentence.split()))
