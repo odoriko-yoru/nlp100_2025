bae6e27a52f75ccdc6ef6fae7a0f522ddc503e07
odoriko-yoru <im.shu.hawking314@gmail.com>
2025-05-09 22:17:55 +0900

    chapter2 #5

---
+++ answer/chapter2/14.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+import pandas as pd
+
+path = os.environ.get("DATA_DIR")
+filename = Path("popular-names.txt")
+
+df = pd.read_csv(path / filename, sep="\t", header=None)
+print(df.iloc[:10, 0])
